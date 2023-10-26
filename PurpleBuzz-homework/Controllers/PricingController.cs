1561b9fdbf0bb1991a57ae7b10d8ccf5e890dbc8
Eziz Rehimov <fantastig@bk.ru>
2023-10-26 23:16:16 +0400

    ViewImports & pricing module completed

---
+++ PurpleBuzz-homework/Controllers/PricingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PurpleBuzz_homework.Controllers
+{
+    public class PricingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
