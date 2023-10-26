459b7b3a23e26f35441239bc43558c4eb532bfb2
Eziz Rehimov <fantastig@bk.ru>
2023-10-26 22:12:12 +0400

    home module completed

--- PurpleBuzz-homework/Controllers/HomeController.cs
+++ PurpleBuzz-homework/Controllers/HomeController.cs
@@ -11,18 +11,18 @@ namespace PurpleBuzz_homework.Controllers
 
             var projectComponents = new List<ProjectComponents>
             {
-                new ProjectComponents(1,"UI/UX test","UI/UX description test",""),
-                new ProjectComponents(2,"Social Media test","Social Media description test",""),
-                new ProjectComponents(3,"Marketing test","Marketing description test",""),
-                new ProjectComponents(4,"Graphic test","Graphic description test",""),
+                new ProjectComponents(1,"UI/UX test","UI/UX description test","/assets/img/services-01.jpg"),
+                new ProjectComponents(2,"Social Media test","Social Media description test","/assets/img/services-02.jpg"),
+                new ProjectComponents(3,"Marketing test","Marketing description test","/assets/img/services-03.jpg"),
+                new ProjectComponents(4,"Graphic test","Graphic description test","/assets/img/services-04.jpg"),
             };
 
             var projectCategories = new List<ProjectCategories>
             {
-                new ProjectCategories(1,"Social Media test","Social Media description test",""),
-                new ProjectCategories(2,"Web marketing test","Web marketing description test",""),
-                                new ProjectCategories(3,"R & D test","R & D description test",""),
-                new ProjectCategories(4,"Branding test","Branding description test",""),
+                new ProjectCategories(1,"Social Media test","Social Media description test","/assets/img/recent-work-01.jpg"),
+                new ProjectCategories(2,"Web marketing test","Web marketing description test","/assets/img/recent-work-02.jpg"),
+                new ProjectCategories(3,"R & D test","R & D description test","/assets/img/recent-work-03.jpg"),
+                new ProjectCategories(4,"Branding test","Branding description test","/assets/img/recent-work-05.jpg"),
             };
 
 
