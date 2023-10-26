f773a87318698a3ad6e46acc703da88039cae110
Eziz Rehimov <fantastig@bk.ru>
2023-10-26 22:33:17 +0400

    about module completed

---
+++ PurpleBuzz-homework/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurpleBuzz_homework.Models;
+
+namespace PurpleBuzz_homework.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            var projectTeamMembers = new List<ProjectTeamMembers>
+            {
+                new ProjectTeamMembers(1,"Birinci shexs test","Business Dev description test","/assets/img/team-01.jpg"),
+                new ProjectTeamMembers(2,"Birinci shexs test","Marketing Dev description test","/assets/img/team-02.jpg"),
+                new ProjectTeamMembers(3,"Birinci shexs test","Simple Dev description test","/assets/img/team-03.jpg")
+            };
+
+            return View(projectTeamMembers);
+        }
+    }
+}
---
+++ PurpleBuzz-homework/Models/ProjectTeamMembers.cs
@@ -0,0 +1,20 @@
+﻿namespace PurpleBuzz_homework.Models
+{
+    public class ProjectTeamMembers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+     
+        public ProjectTeamMembers(int id, string name, string description, string imagePath)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ImagePath = imagePath;
+        }
+    }
+}
