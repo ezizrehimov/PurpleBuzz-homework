d905ff9b31e476a11e2a257f36922f8a55d38205
Eziz Rehimov <fantastig@bk.ru>
2023-10-26 23:08:56 +0400

    contact module completed

---
+++ PurpleBuzz-homework/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurpleBuzz_homework.Models;
+
+namespace PurpleBuzz_homework.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            var projectContactInfos = new List<ProjectContactInfo>
+            {
+                new ProjectContactInfo(1,"Mr ad soyad test1","Media uchun test","055-555-55-55","bx-news"),
+                new ProjectContactInfo(2,"Mrs ad soyad test2","Texnikal uchun test","077-777-77-77", "bx-laptop"),
+                new ProjectContactInfo(3,"Mr ad soyad test3","Odemeler uchun test","010-100-10-10", "bx-money")
+            };
+
+            return View(projectContactInfos);
+        }
+    }
+}
---
+++ PurpleBuzz-homework/Models/ProjectContactInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace PurpleBuzz_homework.Models
+{
+    public class ProjectContactInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Position { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Icon { get; set; }
+
+        public ProjectContactInfo(int id, string name, string position, string phoneNumber, string icon)
+        {
+            Id = id;
+            Name = name;
+            Position = position;
+            PhoneNumber = phoneNumber;
+            Icon = icon;
+        }
+    }
+}
